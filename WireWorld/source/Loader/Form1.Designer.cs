41fb00079a40d495dc24f5059d6246b61a306205
mvxxx <mpatrycjusz@interia.pl>
2016-12-23 18:06:06 +0100

    2.2
    Initial window works

--- WireWorld/source/Loader/Form1.Designer.cs
+++ WireWorld/source/Loader/Form1.Designer.cs
@@ -30,12 +30,12 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.TBa2 = new System.Windows.Forms.TextBox();
+            this.TBd2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.TBa1 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.TBd1 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
@@ -65,20 +65,23 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Start a simulation";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // TBa2
             // 
-            this.textBox1.Location = new System.Drawing.Point(276, 92);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 2;
+            this.TBa2.Location = new System.Drawing.Point(276, 92);
+            this.TBa2.Name = "TBa2";
+            this.TBa2.Size = new System.Drawing.Size(100, 20);
+            this.TBa2.TabIndex = 2;
+            this.TBa2.TextChanged += new System.EventHandler(this.TBa2_TextChanged);
             // 
-            // textBox2
+            // TBd2
             // 
-            this.textBox2.Location = new System.Drawing.Point(276, 141);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 3;
+            this.TBd2.Location = new System.Drawing.Point(276, 141);
+            this.TBd2.Name = "TBd2";
+            this.TBd2.Size = new System.Drawing.Size(100, 20);
+            this.TBd2.TabIndex = 3;
+            this.TBd2.TextChanged += new System.EventHandler(this.TBd2_TextChanged);
             // 
             // label2
             // 
@@ -90,12 +93,13 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "Ammount (X/Y)";
             // 
-            // textBox3
+            // TBa1
             // 
-            this.textBox3.Location = new System.Drawing.Point(160, 92);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 5;
+            this.TBa1.Location = new System.Drawing.Point(160, 92);
+            this.TBa1.Name = "TBa1";
+            this.TBa1.Size = new System.Drawing.Size(100, 20);
+            this.TBa1.TabIndex = 5;
+            this.TBa1.TextChanged += new System.EventHandler(this.TBa1_TextChanged);
             // 
             // label3
             // 
@@ -107,12 +111,13 @@
             this.label3.TabIndex = 6;
             this.label3.Text = "/";
             // 
-            // textBox4
+            // TBd1
             // 
-            this.textBox4.Location = new System.Drawing.Point(160, 141);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 7;
+            this.TBd1.Location = new System.Drawing.Point(160, 141);
+            this.TBd1.Name = "TBd1";
+            this.TBd1.Size = new System.Drawing.Size(100, 20);
+            this.TBd1.TabIndex = 7;
+            this.TBd1.TextChanged += new System.EventHandler(this.TBd1_TextChanged);
             // 
             // label4
             // 
@@ -186,12 +191,12 @@
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.TBd1);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.TBa1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TBd2);
+            this.Controls.Add(this.TBa2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -205,12 +210,12 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox TBa2;
+        private System.Windows.Forms.TextBox TBd2;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox TBa1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox TBd1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
--- WireWorld/source/Loader/Form1.cs
+++ WireWorld/source/Loader/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -36,5 +37,70 @@ namespace Loader
         {
             System.Diagnostics.Process.Start("https://github.com/mvxxx/WireWorld");
         }
+
+        private void TBa1_TextChanged(object sender, EventArgs e)
+        {
+            TBa2.Text = TBa1.Text;
+        }
+
+        private void TBa2_TextChanged(object sender, EventArgs e)
+        {
+            TBa1.Text = TBa2.Text;
+        }
+
+        private void checkValueFromTBa()
+        {
+            int parsedValue;
+            if (!int.TryParse(TBa1.Text, out parsedValue) || parsedValue < 1 || parsedValue > 100000)
+            {
+                MessageBox.Show("You need to enter integer from <1;100.000> in ammount textboxes.");
+            }
+        }
+
+        private void TBd1_TextChanged(object sender, EventArgs e)
+        {
+            TBd2.Text = TBd1.Text;
+        }
+
+        private void TBd2_TextChanged(object sender, EventArgs e)
+        {
+            TBd1.Text = TBd2.Text;
+        }
+
+        private void checkValueFromTBd()
+        {
+            int parsedValue;
+            if (!int.TryParse(TBa1.Text, out parsedValue) || parsedValue < 1 || parsedValue > 200)
+            {
+                MessageBox.Show("You need to enter integer from <1;200> in dimension textboxes.");
+            }
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            checkValueFromTBd();
+            checkValueFromTBa();
+
+
+            if (TBd1.Text == "" || TBa1.Text == "") MessageBox.Show("You need to fill all textboxes.");
+            else
+            {
+                try
+                {
+                    File.WriteAllText("source/Loader/data/options.txt", "WireWorld" + Environment.NewLine + TBa1.Text + " " + TBa2.Text + Environment.NewLine + TBd1.Text + " " + TBd2.Text);
+                    var game = new System.Diagnostics.Process();
+                    game.StartInfo.FileName = "WireWorld.exe"; 
+                    game.Start();
+                    this.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred - please contact with author of project.");
+                }
+            }
+        }
+
     }
 }
